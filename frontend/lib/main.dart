22c1f8860374d9e2618979757fcaf07a3123a7f1
jefftrojan <j.dauda@alustudent.com>
2023-10-17 19:27:15 +0200

    sign up > signin overflow fix

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -29,12 +29,7 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       title: '',
-<<<<<<< Updated upstream
       home: SplashScreenWidget(),
-       
-=======
-      home: CyclePredictionScreen(),
->>>>>>> Stashed changes
     );
   }
 }
