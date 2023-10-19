cde897752475caee3c0893cbba682d328c30ad5f
jefftrojan <j.dauda@alustudent.com>
2023-10-19 08:43:18 +0200

    rm model.pynb

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -20,7 +20,6 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp();
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
