b402271162e14626e1f3f0f6ddda6432e97cb93a
sunday <sarahsmoses4@gmail.com>
2023-10-30 19:03:38 +0200

    splash route validate

--- frontendsrc/lib/brandkit/splash.dart
+++ frontendsrc/lib/brandkit/splash.dart
@@ -1,6 +1,6 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:frontendsrc/assets/screens/onboarding.dart';
+import 'package:frontendsrc/view/onboarding.view.dart';
 
 import 'colors.dart';
 
